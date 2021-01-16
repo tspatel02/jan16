d8d92a15c2b9b45e80b6ca7b116c2b6022028a08
tspatel02 <tspatel02>
2021-01-16 14:34:48 -0600

    test

--- jan16/src/jan16/Jan16.java
+++ jan16/src/jan16/Jan16.java
@@ -17,7 +17,7 @@ public class Jan16 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("hi");
+        System.out.println("hi friend");
     }
     
 }
